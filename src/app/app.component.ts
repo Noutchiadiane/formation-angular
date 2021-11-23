184276894a3c14d1bb71b122562c4b506b9c8293
aloedeveloper1 <61685426+Noutchiadiane@users.noreply.github.com>
2021-11-23 16:35:20 +0100

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'formation';
+  isAuth = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
 }
