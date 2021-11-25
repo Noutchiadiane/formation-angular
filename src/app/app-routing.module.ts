9ed1792bc7ff26e822edd8675ccd9bc4a4bfe3d4
aloedeveloper1 <61685426+Noutchiadiane@users.noreply.github.com>
2021-11-25 07:22:24 +0100

    handle guard in appareil component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { AuthComponent } from './auth/auth.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth.guard';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 
 const appRoutes: Routes = [
-  { path: 'appareils', component: AppareilViewComponent },
-  { path: 'appareils/:id', component: SingleAppareilComponent },
+  {
+    path: 'appareils',
+    canActivate: [AuthGuard],
+    component: AppareilViewComponent,
+  },
+  {
+    path: 'appareils/:id',
+    canActivate: [AuthGuard],
+    component: SingleAppareilComponent,
+  },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: FourOhFourComponent },
