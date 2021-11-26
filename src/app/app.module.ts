db2ae8ea7013c680db0f5fa8004c74bd17a50a09
aloedeveloper1 <61685426+Noutchiadiane@users.noreply.github.com>
2021-11-26 06:23:32 +0100

    handle http client bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,7 +37,7 @@ import { NewUserComponent } from './new-user/new-user.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClient,
+    HttpClientModule,
   ],
   providers: [AppareilService, AuthService, UserService],
   bootstrap: [AppComponent],
