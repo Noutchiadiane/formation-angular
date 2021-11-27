702fa832760a0e826d54c2874809dd4ef90ea7fd
aloedeveloper1 <61685426+Noutchiadiane@users.noreply.github.com>
2021-11-27 14:56:19 +0100

    getsion des livres

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -7,35 +7,54 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
-  styleUrls: ['./book-form.component.css']
+  styleUrls: ['./book-form.component.css'],
 })
 export class BookFormComponent implements OnInit {
-
   bookForm!: FormGroup;
+  fileIsUploading = false;
+  fileUrl!: string;
+  fileUploaded = false;
 
-  constructor(private formBuilder: FormBuilder, private booksService: BooksService,
-              private router: Router) { }
-              
+  constructor(
+    private formBuilder: FormBuilder,
+    private booksService: BooksService,
+    private router: Router
+  ) {}
+
   ngOnInit() {
     this.initForm();
   }
-  
+
   initForm() {
     this.bookForm = this.formBuilder.group({
       title: ['', Validators.required],
       author: ['', Validators.required],
-      synopsis: ''
+      synopsis: '',
     });
   }
-  
+
   onSaveBook() {
     const title = this.bookForm.get('title')!.value;
     const author = this.bookForm.get('author')!.value;
     const synopsis = this.bookForm.get('synopsis')!.value;
     const newBook = new Book(title, author);
     newBook.synopsis = synopsis;
+    if (this.fileUrl && this.fileUrl !== '') {
+      newBook.photo = this.fileUrl;
+    }
     this.booksService.createNewBook(newBook);
     this.router.navigate(['/books']);
+  }
+  onUploadFile(file: File) {
+    this.fileIsUploading = true;
+    this.booksService.uploadFile(file).then((url: string) => {
+      this.fileUrl = url;
+      this.fileIsUploading = false;
+      this.fileUploaded = true;
+    });
+  }
+  detectFiles(event) {
+    this.onUploadFile(event.target.files[0]);
   }
 }
 
